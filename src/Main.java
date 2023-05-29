558eaf180ca7048384e812ece237ac6cb1e707a6
Phuc1112004 <115718926+Phuc1112004@users.noreply.github.com>
2023-05-29 23:13:21 +0700

    assignment

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            JSONManagement jsonManagement = new JSONManagement();
+
+            jsonManagement.readJSONPerson();
+
+//            jsonManagement.readJSONFromAPI();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
